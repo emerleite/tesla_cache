ccb11c90a740ffc0fd6dcec81bc7e9d4ec49ebac
Emerson Macedo <emerleite@gmail.com>
2017-11-07 19:22:48 -0200

    Preparing for hex. Closes #3

--- mix.exs
+++ mix.exs
@@ -1,17 +1,29 @@
 defmodule TeslaCache.Mixfile do
   use Mix.Project
 
+  @description """
+  TeslaCache is a middleware for Elixir Tesla HTTP Client
+  """
+
+  @project_url "https://github.com/emerleite/tesla_cache"
+
   def project do
     [
       app: :tesla_cache,
       version: "0.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
+      description: @description,
+      source_url: @project_url,
+      homepage_url: @project_url,
+      package: package(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
+      name: "TeslaCache",
       docs: [
         main: "readme",
-        extras: ["README.md"]
+        extras: ["README.md"],
+        source_url: @project_url
       ]
     ]
   end
@@ -29,10 +41,18 @@ defmodule TeslaCache.Mixfile do
     [
       {:cachex, "~> 2.1"},
       {:tesla, "~> 0.9.0"},
-      {:excoveralls,    "~> 0.7.2",  only: :test}
+      {:excoveralls,    "~> 0.7.2",  only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+  defp package do
+    [
+      maintainers: ["Emerson Macedo"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/emerleite/tesla_cache"}
     ]
   end
 end
